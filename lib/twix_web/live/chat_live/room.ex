7918b16a25efc57bf5158086bc91596e1318afdc
ryuzaki <ryuzaki>
2021-06-19 09:44:37 +0530

    show single room

--- lib/twix_web/live/chat_live/room.ex
+++ lib/twix_web/live/chat_live/room.ex
@@ -1,7 +1,8 @@
 defmodule TwixWeb.ChatLive.Room do
   use TwixWeb, :live_view
 
-  def mount(%{"name" => room_name}, %{"current_user_id" => current_user_id}, socket) do
-    {:ok, socket}
+  def mount(%{"name" => room_name}, %{"current_user_id" => _current_user_id}, socket) do
+    {:ok, assign(socket, current_room: room_name)}
   end
+
 end
