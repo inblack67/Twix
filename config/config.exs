b6a25da35ad7fb9dd7f503a1e850192c419d1a98
ryuzaki <ryuzaki>
2021-06-20 15:15:31 +0530

    ssl db and http

--- config/config.exs
+++ config/config.exs
@@ -27,7 +27,7 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# config :twix, Twix.Repo, ssl: true
+config :twix, Twix.Repo, ssl: true
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
